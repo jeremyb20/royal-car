d7444230ad60a64f0213b99f1793b83eb8c6608a
Jeremy <jbaccab@ucenfotec.ac.cr>
2018-05-04 04:14:10 -0600

    sidebar close sesion

--- public/components/directives/sidebar/sidebar.directive.js
+++ public/components/directives/sidebar/sidebar.directive.js
@@ -4,23 +4,52 @@
   .module('royal-car')
   .directive('sideMenu', sideMenu)
 
-  sideMenu.$inject = ['loginService'];
+  sideMenu.$inject = ['$state', 'loginService'];
   
-  function sideMenu(loginService){
+  function sideMenu($state, loginService){
 
-    let headlineController = function () {
+    let sideBarController = function () {
       let vm = this;
 
      let userAuth = loginService.getAuthUser();
     
       vm.userInfo = userAuth;
+
+      angular.element('#btnClose').on('click', function () {
+        swal("Desea cerrar la sesión?", {
+                buttons: {
+                    cancel: "Cancelar",
+                    cerrarSesion: {
+                        text: "Cerrar sesión",
+                        value: "cerrarSesion",
+                    },
+                },
+            })
+            .then((value) => {
+                switch (value) {
+                    case "cerrarSesion":
+                        loginService.logOut();
+                        $state.go('logIn');
+                        swal({
+                            title: "Sesión cerrada correctamente",
+                            text: "Se ha finalizado su sesión correctamente",
+                            icon: "success",
+                            button: "Aceptar",
+                        });
+                        break;
+
+                    default:
+                        break;
+                }
+            })
+    })
     }
 
     let sidebar = {
       templateUrl: '/components/directives/sidebar/sidebar.view.html',
       restrict: 'EA',
       require: "ngClick",
-      controller: headlineController,
+      controller: sideBarController,
       controllerAs: 'vm',
     };
 
